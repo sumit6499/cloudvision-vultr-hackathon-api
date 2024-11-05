435b20c9f6fc00ad0f17a8cd4234ce0070d19fad
Sumit <sumitpadalkar53@gmail.com>
2024-11-05 11:34:21 +0530

    init: cloudvision backend

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+
+
+app.get('/', (c) => {
+  return c.json(successMsg({
+    success:true,
+    msg:"Hello form cloudvision server"
+  }),200)
+})
+
+app.notFound((c)=>{
+  return c.html("<h1>Route not found Please check route endpoint</h1>",404)
+})
+
+export default {
+  port:process.env.PORT || 3000,
+  fetch:app.fetch,
+  maxRequestBodySize:1024*1024*200,
+}
